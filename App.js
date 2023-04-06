e0c85bcbd17591b9b64e124a32b1ff7012d5525a
Marlon Roches <roches856@gmail.com>
2023-04-06 11:28:22 -0600

    Fetching data from API

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, SafeAreaView, Text, TouchableOpacity, TextInput, ScrollView, Platform } from 'react-native';
+import Timer from './Components/Timer';
 
-export default function App() {
+const App = () => {
+  const [timers, setTimers] = useState([]);
+  const scrollViewRef = useRef();
+
+  const handleAddTimer = () => {
+    const newTimer = <Timer key={timers.length.toString()} />;
+    setTimers([...timers, newTimer]);
+    scrollViewRef.current.scrollToEnd({ animated: true });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={[styles.container, Platform.select({android: {paddingTop: '3%'}})]}>
+        <ScrollView ref={scrollViewRef}>
+          {timers.map((timer) => {
+            return (
+              <>
+                <Separador id={timer.key} />
+                {timer}
+              </>
+            );
+          })}
+        </ScrollView>
+        <TouchableOpacity style={styles.button} onPress={handleAddTimer}>
+          <Text style={styles.buttonText}>Agregar Timer</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = {
+  container: {
+    maxHeight: '100%', // establecer altura máxima del contenedor
+  },
+  button: {
+    backgroundColor: '#5cb85c',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+};
+
+function Separador({ id }) {
+  return (
+    <>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <View style={{ flex: 1, height: 1, backgroundColor: 'black' }} />
+        <View>
+          <Text style={{ width: 50, textAlign: 'center' }}>{id}</Text>
+        </View>
+        <View style={{ flex: 1, height: 1, backgroundColor: 'black' }} />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ Components/Timer.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, TextInput , StyleSheet} from 'react-native';
+
+export default Timer = () => {
+  const [startTime, setStartTime] = useState(null);
+  const [queueEndTime, setQueueEndTime] = useState(null);
+  const [attendEndTime, setAttendEndTime] = useState(null);
+  const [totalTime, setTotalTime] = useState(null);
+  const [nickname, setNickname] = useState('');
+
+  const startTimer = () => {
+    setStartTime(new Date());
+    setQueueEndTime(null)
+    setAttendEndTime(null)
+    setTotalTime(null)
+  };
+
+  const markQueueEnd = () => {
+    setQueueEndTime(new Date());
+  };
+
+  const markAttendEnd = () => {
+    setAttendEndTime(new Date());
+  };
+
+  const calculateTotalTime = () => {
+    const queueTime = queueEndTime - startTime;
+    const attendTime = attendEndTime - queueEndTime;
+    const totalTime = queueTime + attendTime;
+    setTotalTime(totalTime);
+  };
+
+  const renderTimer = () => {
+    if (!startTime) {
+      return <Text>Presione el botón "Inicio" para comenzar el temporizador.</Text>;
+    } else if (!queueEndTime) {
+      return <Text>Presione el botón "Fin de cola" cuando finalice la espera en la cola.</Text>;
+    } else if (!attendEndTime) {
+      return <Text>Presione el botón "Fin de atender" cuando finalice la atención.</Text>;
+    } else {
+      return <Text>Tiempo total de atención: {totalTime/1000} segundos.</Text>;
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="Ingrese el nombre del temporizador"
+        onChangeText={(value) => setNickname(value)}
+        value={nickname}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={startTimer}>
+          <Text style={styles.buttonText}>Inicio</Text>
+          <Text>StarTime: {formatTime(startTime)}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.redButton]} onPress={markQueueEnd}>
+          <Text style={styles.buttonText}>Fin de cola</Text>
+          <Text>Fin Cola: {formatTime(queueEndTime)}</Text>
+          
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.orangeButton]} onPress={markAttendEnd}>
+          <Text style={styles.buttonText}>Fin de atender</Text>
+          <Text>Tiempo Atencion: {formatTime(attendEndTime)}</Text>
+
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.blueButton]} onPress={calculateTotalTime}>
+          <Text style={styles.buttonText}>Tiempo de atención</Text>
+        </TouchableOpacity>
+      </View>
+      {renderTimer()}
+    </View>
+  );
+};
+
+function formatTime(date) {
+    if (date === null || date === undefined) {
+      return '0';
+    }
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  
+
+
+const styles = StyleSheet.create ({
+    // container: {
+    //   alignItems: 'center',
+    //   marginTop: 60
+    // },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: "center",
+      marginVertical: 20,
+      width: '100%',
+    //   backgroundColor: "re d"
+    },
+    button: {
+      backgroundColor: '#5cb85c',
+      padding: 10,
+      borderRadius: 5,
+      marginBottom: 10,
+      width: "25%",
+    },
+    buttonText: {
+      color: '#fff',
+      textAlign: 'center',
+      fontWeight: 'bold',
+    },
+    redButton: {
+      backgroundColor: '#d9534f',
+    },
+    orangeButton: {
+      backgroundColor: '#f0ad4e',
+    },
+    blueButton: {
+      backgroundColor: '#337ab7',
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderRadius: 5,
+      padding: 10,
+    //   marginBottom: 5,
+      width: '80%',
+    },
+  });
+  
